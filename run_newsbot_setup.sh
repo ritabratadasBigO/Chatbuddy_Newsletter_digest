246572e42caa8728eb5934f46db2a21d51c923b8
niteowl1986 <ritabratadas12@gmail.com>
2025-06-14 02:55:07 +0530

    Shell script to run in local environment automatically.

---
+++ run_newsbot_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+# Load environment variables
+source ~/.zshrc
+
+# Activate the conda environment
+source /Users/ritabrata.das/miniconda3/bin/activate newschat-env
+
+# Run the setup script
+python /Users/ritabrata.das/Daily-News-Feed/newsbot_indexer/Chatbuddy_Newsletter_digest/chat_newsbot_setup.py
