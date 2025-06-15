22dbd5917f1314b04b235745da58433cacc783b2
niteowl1986 <49530755+niteowl1986@users.noreply.github.com>
2025-06-15 20:24:46 +0530

    Updated shell script to validate remote origin.

--- run_newsbot_indexer.sh
+++ run_newsbot_indexer.sh
@@ -10,6 +10,12 @@ pip install -r requirements.txt
 git config --global user.name "niteowl1986"
 git config --global user.email "49530755+niteowl1986@users.noreply.github.com"
 
+# Ensure origin is configured
+git remote get-url origin 2> /dev/null
+if [ $? -ne 0 ]; then
+  git remote add origin https://${GITHUB_PAT}@github.com/niteowl1986/Chatbuddy_Newsletter_digest.git
+fi
+
 # Ensure we are on the main branch to avoid detached HEAD issues
 git checkout main
 git pull origin main
