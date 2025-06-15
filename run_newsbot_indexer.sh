6cef9abfcd2a12cc5a4b2d877bd305617c43221f
niteowl1986 <ritabratadas12@gmail.com>
2025-06-15 18:33:44 +0530

    Added git credentials

--- run_newsbot_indexer.sh
+++ run_newsbot_indexer.sh
@@ -6,5 +6,9 @@ python --version
 # Install dependencies (if needed)
 pip install -r requirements.txt
 
+# Configure Git user details
+git config --global user.name "niteowl1986"
+git config --global user.email "49530755+niteowl1986@users.noreply.github.com"
+
 # Run the setup script
 python chat_newsbot_setup.py
