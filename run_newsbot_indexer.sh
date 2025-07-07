5353d1715b05a6e5d1eb931ce4efc13401f711b2
niteowl1986 <49530755+niteowl1986@users.noreply.github.com>
2025-07-08 00:47:11 +0530

    Changed repo owner name.

--- run_newsbot_indexer.sh
+++ run_newsbot_indexer.sh
@@ -7,13 +7,13 @@ python --version
 pip install -r requirements.txt
 
 # Configure Git user details
-git config --global user.name "niteowl1986"
-git config --global user.email "49530755+niteowl1986@users.noreply.github.com"
+git config --global user.name "ritabratadasBigO"
+git config --global user.email "49530755+ritabratadasBigO@users.noreply.github.com"
 
 # Ensure origin is configured
 git remote get-url origin 2> /dev/null
 if [ $? -ne 0 ]; then
-  git remote add origin https://${GITHUB_PAT}@github.com/niteowl1986/Chatbuddy_Newsletter_digest.git
+  git remote add origin https://${GITHUB_PAT}@github.com/ritabratadasBigO/Chatbuddy_Newsletter_digest.git
 fi
 
 # Ensure we are on the main branch to avoid detached HEAD issues
